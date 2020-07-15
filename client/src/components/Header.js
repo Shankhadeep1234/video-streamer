30d4d749e5565ceef61afed924ace015617db1db
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-07-15 20:58:46 +0530

    added auth with google

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import GoogleAuth from "./GoogleAuth";
+
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item" style={{ color: "red" }}>
-        <i className="forward icon"></i>
-        Streamer
+        <h2>
+          <i className="video icon"></i>
+          Streamer
+        </h2>
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
           All Streams
         </Link>
+        <GoogleAuth />
       </div>
     </div>
   );
